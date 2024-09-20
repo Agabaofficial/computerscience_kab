f991d37a8aa711ed5b961c6aa4107ec6ce4ee0d4
Agaba Olivier <125471253+Agabaofficial@users.noreply.github.com>
2024-09-20 21:22:08 +0300

    Updated head

--- olivier.php
+++ olivier.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome Form</title>
+    <title>first php program at campus</title>
     <style>
         * {
             margin: 0;

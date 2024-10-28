a772be0a4ab47fe3313ebaa25f1da9d8be850274
Agaba Olivier <125471253+Agabaofficial@users.noreply.github.com>
2024-10-28 15:51:01 +0300

    first commit

---
+++ php-attendance/pages/home.php
@@ -0,0 +1,6 @@
+<div class="page-title">Student Attendance Management System</div>
+<hr>
+<p> Made with Love by AGABA OLIVIER </p>
+
+<center>Within the Attendance Form, users are prompted to initially select the class and date before the system lists all the students. Users can then easily check the attendance status for each student. The form includes a convenient "Check/Uncheck All" feature for each attendance status, allowing users to mark or unmark all listed student attendance with a single click. If the form is submitted with any unmarked attendance status, it will display a message and halt the submission of attendance data.
+</center>

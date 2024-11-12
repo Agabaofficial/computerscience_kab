57e3c03a9fd4960f1f7f75e566384266f97e6ca9
Agaba Olivier <125471253+Agabaofficial@users.noreply.github.com>
2024-11-12 14:36:34 +0300

    Update db-connect.php

--- php-attendance/db-connect.php
+++ php-attendance/db-connect.php
@@ -2,14 +2,14 @@
 // Hostname
 $host = "localhost";
 // Username
-$uname = "root";
+$username = "root";
 // Password
 $pw = "";
 // Database Name
 $dbname = "simple_attendance_db";
 
 try{
-    $conn = new MySQLi($host, $uname, $pw, $dbname);
+    $conn = new MySQLi($host, $username, $pw, $dbname);
 }catch(Exception $e){
     echo "Database Connection Failed: <br>";
     print_r($e->getMessage());
